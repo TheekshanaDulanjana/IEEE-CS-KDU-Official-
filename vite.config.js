b7e158da3870958d6169f62926382ebaf1f556bf
OPT Dulanjana <140092772+TheekshanaDulanjana@users.noreply.github.com>
2025-07-13 15:28:43 +0530

    Test Google Form III

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  },
-  server: {
-    historyApiFallback: true // ✅ Only affects local dev
-  }
 })
